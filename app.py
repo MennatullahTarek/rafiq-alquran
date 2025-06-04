44762bd4aaa6b27bf1459716b99f9ddaca06ec75
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-04 16:30:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ st.set_page_config(page_title="رفيق القرآن", layout="wide")
 st.sidebar.title("القائمة الرئيسية")
 page = st.sidebar.radio("اختر الصفحة:", [
     "لوحة التحكم",
+    "التلاوة"
 ])
 
 if page == "لوحة التحكم":
     import safahat.dash_01 as dash
     dash.app()
+
+elif page == "التلاوة":
+    import safahat.tilawa_02 as tilawa
+    tilawa.app()
