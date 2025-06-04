2569d56ae0b47d225eccaa6300298994597ec2aa
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-04 16:12:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="رفيق القرآن", layout="wide")
 
-st.sidebar.title("📚 القائمة الرئيسية")
+st.sidebar.title("القائمة الرئيسية")
 page = st.sidebar.radio("اختر الصفحة:", [
-    "📊 لوحة التحكم",
+    "لوحة التحكم",
 ])
 
-if page == "📊 لوحة التحكم":
+if page == "لوحة التحكم":
     import safahat.dash_01 as dash
     dash.app()
