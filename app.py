7e040ee97517beee6d2e30b1be0decc084aac1a9
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-05 15:16:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="رفيق القرآن", layout="wide")
 st.sidebar.title("القائمة الرئيسية")
 page = st.sidebar.radio("اختر الصفحة:", [
     "لوحة التحكم",
-    "لاستماع",
+    "الاستماع",
     "مُخطط الحفظ"
 ])
 
@@ -14,7 +14,7 @@ if page == "لوحة التحكم":
     import safahat.dash_01 as dash
     dash.app()
 
-elif page == "التلاوة":
+elif page ==  "الاستماع":
     import safahat.estimaa_02 as estimaa
     estimaa.app()
 
