973ef58ef72c636cb60146581847563cba1835c1
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-05 21:14:00 +0300

    Update tafsir_05.py

--- safahat/tafsir_05.py
+++ safahat/tafsir_05.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import requests
-from deep_translator import GoogleTranslator
-from bs4 import BeautifulSoup
 
-# دالة لجلب التفسير من Quran.com API
-def get_tafsir_quran_api(surah, ayah, tafsir_id=91):  # Tafsir Ibn Kathir English (abridged)
+
+def get_tafsir_quran_api(surah, ayah, tafsir_id=91):  
     url = f"https://api.quran.com/api/v4/tafsirs/{tafsir_id}/by_ayah/{surah}:{ayah}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -16,20 +14,6 @@ def get_tafsir_quran_api(surah, ayah, tafsir_id=91):  # Tafsir Ibn Kathir Englis
         return "❌ فشل في الاتصال بالـ API."
 
 
-
-def clean_html(raw_html):
-    soup = BeautifulSoup(raw_html, "html.parser")
-    return soup.get_text(separator="\n")
-
-def translate_to_arabic(text):
-    try:
-        clean_text = clean_html(text)
-        return GoogleTranslator(source='en', target='ar').translate(clean_text)
-    except:
-        return "⚠️ فشل في الترجمة."
-
-
-# واجهة المستخدم
 def app():
     st.title("📖 تفسير آية من القرآن الكريم")
     
@@ -38,13 +22,10 @@ def app():
     
     if st.button("📚 عرض التفسير"):
         st.info("⏳ جاري جلب التفسير من API...")
-        tafsir_en = get_tafsir_quran_api(surah, ayah)
-        st.subheader("📘 التفسير (بالإنجليزية):")
-        st.write(tafsir_en)
+        tafsir = get_tafsir_quran_api(surah, ayah)
         
-        st.subheader("📗 الترجمة للعربية:")
-        tafsir_ar = translate_to_arabic(tafsir_en)
-        st.write(tafsir_ar)
+        st.subheader("📗  التفسير:")
+        st.markdown(tafsir, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     app()
