b960d39530f60d9979606da459bf0bb454976422
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-05 07:01:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import safahat.hifz_planner_03 as hifz
+
 
 st.set_page_config(page_title="رفيق القرآن", layout="wide")
 
@@ -19,4 +19,5 @@ elif page == "التلاوة":
     tilawa.app()
 
 elif page == "مُخطط الحفظ":
+    import safahat.hifz_planner_03 as hifz
     hifz.app()
