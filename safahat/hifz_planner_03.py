fa4d4f7468716127527fb2a7e1b811eb02f7ad38
Mennatullah Tarek <83369752+MennatullahTarek@users.noreply.github.com>
2025-06-07 17:54:29 +0300

    Update hifz_planner_03.py

--- safahat/hifz_planner_03.py
+++ safahat/hifz_planner_03.py
@@ -94,6 +94,48 @@ def display_ui(theme):
                 margin-top: 30px;
                 margin-bottom: 10px;
             }}
+
+            
+        
+        .stForm button {{
+            background-color: #388E3C;
+            color: white;
+            font-size: 1rem;
+            border-radius: 8px;
+            padding: 0.4rem 1rem;
+            margin-top: 10px;
+            border: 2px solid #2E7D32;
+            transition: all 0.3s ease;
+            font-weight: bold;
+            cursor: pointer;
+        }}
+    
+        .stForm button:hover {{
+            background-color: #1B5E20;
+            border-color: #1B5E20;
+            transform: scale(1.03);
+        }}
+    
+    
+        
+        .stDownloadButton>button {{
+            background-color: #388E3C;
+            color: white;
+            font-size: 1rem;
+            border-radius: 8px;
+            padding: 0.4rem 1rem;
+            margin-top: 15px;
+            border: 2px solid #2E7D32;
+            transition: all 0.3s ease;
+            font-weight: bold;
+            cursor: pointer;
+        }}
+        
+        .stDownloadButton>button:hover {{
+            background-color: #1B5E20;
+            border-color: #1B5E20;
+            transform: scale(1.03);
+        }}
         </style>
     """, unsafe_allow_html=True)
 
